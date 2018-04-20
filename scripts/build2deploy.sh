44ca9d91be69f07af64ce154dc22bd8911ff7db9
Benjamin <ben.guichard@live.fr>
2018-04-20 17:41:26 +0200

    Fix in build2deploy.sh script the BUILD_ID syntax

--- scripts/build2deploy.sh
+++ scripts/build2deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 BUILD_DIR=$1
 COMMIT=$2
-BUILD_ID="$3#$4"
+BUILD_ID="$3_$4"
 
 PACKER_DIR=$BUILD_DIR/packer
 
@@ -13,6 +13,5 @@ packer.io build -force \
   -var "build_id=$BUILD_ID" \
   $PACKER_DIR/packer.json
 
-#AMI=$(egrep "(eu|us|ap|sa)-(west|central|east|northeast|southeast)-(1|2|3): ami-" /tmp/packer.out | sed 's/.*ami-\([0-9a-f]*\).*/ami-\1/g')
 AMI=`jq -r '.builds[0].artifact_id[-12:]' manifest.json`
 echo $AMI
