63fb5df5557e8a41fb3bf493fac8f7828ed5b54a
Benjamin <ben.guichard@live.fr>
2018-04-24 17:56:16 +0200

    Create SecureGroup, Routing and instance templates

---
+++ terraform/output.tf
@@ -0,0 +1,31 @@
+### VPC ###
+
+output "region" {
+  value = "${var.region}"
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.main.id}"
+}
+
+output "public_subnets" {
+  value = "${aws_subnet.public.id}"
+}
+
+output "sg_http" {
+  value = "${aws_security_group.http.id}"
+}
+
+### FRONTEND ###
+
+output "web_dns" {
+  value = "${aws_instance.web.public_dns}"
+}
+
+output "web_public_ip" {
+  value = "${aws_instance.web.public_ip}"
+}
+
+output "web_private_ip" {
+  value = "${aws_instance.web.private_ip}"
+}
--- terraform/terraform.tfvars
+++ terraform/terraform.tfvars
@@ -7,4 +7,8 @@ region            = "eu-west-3"
 vpc_name          = "demo-vpc"
 base_network      = "10.0.0.0/16"
 public_networks   = "10.0.10.0/24"
-private_networks  = "10.0.20.0/24"
+
+### INSTANCE ###
+
+frontend_name     = "MyHTTP"
+instance_size     = "t2.micro"
