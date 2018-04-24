79f772d7e4dce1e3a93ce70acc8ad1b5822193fa
Benjamin <ben.guichard@live.fr>
2018-04-24 10:35:07 +0200

    Add Packer syntax checking step

--- scripts/build2deploy.sh
+++ scripts/build2deploy.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 set -e
 
+### VARIABLES ###
+
 BUILD_DIR=$1
 COMMIT=$2
 BUILD_ID="$3_$4"
 
 PACKER_DIR=$BUILD_DIR/packer
 
+### PACKER AMI CREATION ###
+
+# Template syntax checking
+$TOOLS_DIR/bin/packer validate \
+  -var "site_dir=$BUILD_DIR/www" \
+  -var "commit=$COMMIT" \
+  -var "build_id=$BUILD_ID" packer.json || exit 1
+
+# Creation and upload of the AMI
 $TOOLS_DIR/bin/packer build -force \
   -var "site_dir=$BUILD_DIR/www" \
   -var "commit=$COMMIT" \
   -var "build_id=$BUILD_ID" \
-  $PACKER_DIR/packer.json
+  $PACKER_DIR/packer.json || exit 1
+
+### TERRAFORM DEPLOYMENT ###
 
 AMI=`jq -r '.builds[0].artifact_id[-12:]' manifest.json`
 echo $AMI
